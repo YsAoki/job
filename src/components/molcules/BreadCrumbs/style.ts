4171cedc55582021e5c1eb110b63b5c86b251d4d
YsAoki <kazuaki.aoki@ysinc.co.jp>
2024-07-27 06:19:14 +0900

    パンくずリストジェネレータコンポーネントの作成

---
+++ src/components/molcules/BreadCrumbs/style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import FlexBox from "../../atoms/FlexBox";
+
+export const SFlexBox = styled(FlexBox)`
+  align-items: center;
+  justify-content: flex-start;
+  gap: 0.75rem;
+  a, p {
+    font-size: 0.75rem;
+  }
+`
